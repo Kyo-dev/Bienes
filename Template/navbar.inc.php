665239fafb4ab99d995ea281d59a6a417f63fcb2
Chris <chris_db@icloud.com>
2019-03-06 15:35:20 -0600

    productos.php

--- Template/navbar.inc.php
+++ Template/navbar.inc.php
@@ -1,6 +1,7 @@
 <div class="services">
         <div class="services__item">
             <h3 class="services__item__title">Casas</h3>
+            
         </div>
         <div class="services__item">
             <h3 class="services__item__title">Terrenos</h3>
